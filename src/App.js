0ebcffa9f85c2c852e8adb95df3b2d7c5118a2d9
amol rautrao <amolrautrao23@gmail.com>
2023-03-20 16:26:44 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,42 @@
+
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Home from './Components/Home';
+import { BrowserRouter, Route, Routes, Navigate, NavLink } from 'react-router-dom';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Enquiry from './Components/Enquiry';
+import Service from './Components/Service';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import PopUpIcon from './Components/PopUpIcon';
+import { useEffect } from 'react';
+import ScrollToTop from './Components/ScrollToTop';
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+    <ScrollToTop/>
+      <Header/>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/home' element={<Home />} />
+          <Route exact path='/about' element={<About />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/enquiry' element={<Enquiry />} />
+          <Route exact path='/service' element={<Service />} />
+          <Route path="*" element={<Navigate to="/" />}
+          />
+        </Routes>
+        <PopUpIcon/>
+        <Footer/>
+      </BrowserRouter>
+
+    </>
+  );
+  
+}
+
+export default App;
---
+++ src/Components/UpDown.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UpDown = () => {
+  return (
+    <>
+      <div className="updown-container">
+        <div className="inc">+</div>
+        <div className="qty">0</div>
+        <div className="dec">-</div>
+      </div>
+    </>
+  )
+}
+
+export default UpDown
